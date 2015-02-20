31e296c67002531004c04e6392205bb02d992c27
Matt Mondok <mattm@edentech.net>
2015-02-19 22:10:59 -0500

    Initial Commit

---
+++ ConstraintsSamples/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConstraintsSamples
+//
+//  Created by Matthew Mondok on 2/19/15.
+//  Copyright (c) 2015 Matthew Mondok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
